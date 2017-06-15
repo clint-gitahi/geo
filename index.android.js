b9137989c619582c692508f3b81e3aa4ceea2ffa
clint-gitahi <clingitahi@gmail.com>
2017-06-15 08:21:09 +0300

    adding styles to marker

--- index.android.js
+++ index.android.js
@@ -30,6 +30,26 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 112, 255, 0.3)',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  marker: {
+    height: 20,
+    width: 20,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 20 / 2,
+    overflow: 'hidden',
+    backgroundColor: '#007AFF'
+  },
 });
 
 export default class Tested extends Component {
@@ -60,7 +80,11 @@ export default class Tested extends Component {
             latitude: 37.78825,
             longitude: -122.4324,
           }}
-        />
+        >
+          <View style={styles.radius}>
+            <View style={styles.marker} />
+          </View>
+        </MapView.Marker>
       </MapView>
     );
   }
